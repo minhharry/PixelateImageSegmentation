b39cbba850de445ad570133938bcc07538c7d541
minhharry <115330593+minhharry@users.noreply.github.com>
2024-01-25 20:36:26 +0700

    add no_mask dataset

--- dataset.py
+++ dataset.py
@@ -7,11 +7,12 @@ from torch.utils.data import Dataset
 from torchvision.transforms import ToTensor
 
 class TorinoAquaDataset(Dataset):
-    def __init__(self, rootdir='torinoaqua') -> None:
+    def __init__(self, rootdir='torinoaqua', no_mask=0.5) -> None:
         super().__init__()
         self.rootdir = rootdir
         self.listdir = os.listdir(rootdir)
         self.transfroms = ToTensor()
+        self.no_mask = no_mask
         
     def __len__(self):
         return len(self.listdir)
@@ -21,7 +22,16 @@ class TorinoAquaDataset(Dataset):
         img = img.convert('RGBA')
         x, y = random.randint(0, img.size[0]-512), random.randint(0, img.size[1]-512)
         label = img.crop((x,y,x+512,y+512))
-
+        input = label.copy()
+        maskbig = Image.new('L', label.size, 0)
+        if random.random()<self.no_mask:
+            input = input.convert('RGB')
+            label = label.convert('RGB')
+            maskbig = maskbig.convert('L')
+            input = self.transfroms(input)
+            label = self.transfroms(label)
+            maskbig = self.transfroms(maskbig)
+            return {'input':input, 'label':label, 'mask':maskbig}
         egdeLength = 400
         x, y = random.randint(0, 512-egdeLength), random.randint(0, 512-egdeLength)
         factor = random.randint(10, 50)
@@ -36,9 +46,7 @@ class TorinoAquaDataset(Dataset):
             draw.ellipse((ex, ey, ex+ew, ey+eh), fill=255)
         temp.putalpha(mask)
 
-        input = label.copy()
         input.paste(temp, (x, y, x+egdeLength, y+egdeLength), temp)
-        maskbig = Image.new('L', label.size, 0)
         maskbig.paste(mask, (x, y, x+egdeLength, y+egdeLength), mask)
 
         input = input.convert('RGB')
@@ -51,8 +59,8 @@ class TorinoAquaDataset(Dataset):
 
 if __name__ == '__main__':
     dataset = TorinoAquaDataset()
-    fig, axes = plt.subplots(3, 3, figsize=(50,20))
-    for i in range(3):
+    fig, axes = plt.subplots(5, 3, figsize=(50,20))
+    for i in range(5):
         data = dataset[i]
         axes[i, 0].set_title('input')
         axes[i, 0].imshow(data['input'].numpy().transpose(1,2,0))
